60f95596e6620b48b401b2d5beac32969f1a8a1f
Benoit Senechal <benoit.senechal@telecom-paris.fr>
2019-12-03 13:31:15 +0100

    Refactoring

--- get_IDlist.py
+++ get_IDlist.py
@@ -3,21 +3,15 @@ import re
 
 r = requests.get("https://www.gutenberg.org/wiki/Category:FR_Genre")
 
-subdir_list = []
-
-for line in r.text.split('\n'):
-    if line.find('(Genre)">') != -1:
-        subdir_list.append(line.split('"')[1])
+subdir_list = re.findall('/wiki/(.*)_\(Genre\)"', r.text, re.MULTILINE)
 
 ebook_id_list = []
 for subdir in subdir_list:
-    r = requests.get("https://www.gutenberg.org/" + subdir)
-    for line in r.text.split('\n'):
-        index_start = line.find('title="ebook:')
-        if index_start != -1:
-            index_stop = line.find('"', index_start + 13, len(line))
-            ebook_id_list.append(int(line[index_start + 13:index_stop]))
+    r = requests.get("https://www.gutenberg.org/wiki/" + subdir + "_(Genre)")
+    e_ids = re.findall('title="ebook:([0-9]+)">', r.text, re.MULTILINE)
+    ebook_id_list += e_ids
+    print(ebook_id_list)
 
+# TODO: Download the books thanks to the ebook id
 #for ebook_id in ebook_id_list:
-#   text = strip_headers(load_etext(2701)).strip()
-# TO BE CONTINUED ...
+#   text = strip_headers(load_etext(ebook_id)).strip()
